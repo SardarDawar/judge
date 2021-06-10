85baa13fa396c5ee13569035a16fd7fd9a1cd2f0
SardarDawar <49003336+SardarDawar@users.noreply.github.com>
2021-06-10 07:34:20 +0500

    Update dev_settings.py

--- oj/dev_settings.py
+++ oj/dev_settings.py
@@ -5,12 +5,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '127.0.0.1',
-        'PORT': 5435,
-        'NAME': "onlinejudge",
-        'USER': "onlinejudge",
-        'PASSWORD': 'onlinejudge'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
